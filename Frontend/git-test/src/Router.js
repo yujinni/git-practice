47de8f2f742df000f058c4ad372011f1d84738d4
이유진 <yjoct13@gmail.com>
2023-02-02 20:55:53 +0900

    Modify: make conflict

--- Frontend/git-test/src/Router.js
+++ Frontend/git-test/src/Router.js
@@ -2,6 +2,10 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Main from "./pages/Main/Main";
+import Main from "./pages/Main/Main";
+import Main from "./pages/Main/Main";
+import Main from "./pages/Main/Main";
+import Main from "./pages/Main/Main";
 
 export default function Router() {
   return (
