a1135d9ec468389b1c75fb2be00c89739333480f
이유진 <yjoct13@gmail.com>
2023-02-02 20:39:17 +0900

    Add:메인 페이지 구현

--- Frontend/git-test/src/Router.js
+++ Frontend/git-test/src/Router.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login/Login";
+import Main from "./pages/Main/Main";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/main" element={<Main />} />
       </Routes>
     </BrowserRouter>
   );
